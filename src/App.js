dbb66a2289fb2d2d68b082af5280f7ee051ce163
krongdew <dewwiisunny14@gmai.com>
2024-02-19 14:31:52 +0700

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import Product from './components/pages/Product'
+import SignUp from './components/pages/SignUp'
 function App() {
   return (
     <>
@@ -10,6 +13,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} /> 
+        <Route path="/services" element={<Services/>} /> 
+        <Route path="/Products" element={<Product/>} /> 
+        <Route path="/sign-up" element={<SignUp/>} /> 
       </Routes>
       </BrowserRouter>
     </>
